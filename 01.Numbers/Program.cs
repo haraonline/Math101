29c9be9d49796eeba0c2d520dc13bea6812153b8
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-03-24 16:46:57 +0100

    Add project files.

---
+++ 01.Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number Sets");
+            Console.WriteLine("Whole Numbers  : { 0, 1, 2, 3 ... }");
+            Console.WriteLine("Natural or Counting Numbers : { 1, 2, 3 ... }");
+            Console.WriteLine("Integers or + and - Numbers : ... { -3, -2, -1, 0, 1, 2, 3 ... }");
+            Console.WriteLine("Rational Numbers: Integers + Fractions: { -6, -7/2, 3/4, 12 ... }");
+            Console.WriteLine("Irrational Numbers: (Only) Real Numbers that cannot be writen as a fraction: 3.14159, 1.414, 0.3333 ... squareRoot of 2, Pi etc");
+            Console.WriteLine("Irrational: all Real Numbers but Rational");
+            Console.WriteLine("Overching Real Numbers: Rational + Irrational: 3.14159, 1.414, 0.3333 ...");            
+        }
+    }
+}
