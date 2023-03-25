d50b998c4629be4a2c3bdb36f189437d537217fa
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-03-25 17:11:04 +0100

    negative numbers

--- 01.Numbers/Program.cs
+++ 01.Numbers/Program.cs
@@ -29,12 +29,33 @@
             Console.WriteLine("Negative numbers are numbers that are less than zero. \nThey are also called negative integers or negative whole numbers.");
             Console.WriteLine("Negative numbers are used to represent the opposite of a positive number.");
             Console.WriteLine("When any number is multiplied by -1, the result is the opposite of the original number.");
-            Console.WriteLine("For example, if you multiply 5 by -1, the result is -5.");
-            int someNumber = 5;
+            Console.WriteLine("For example, if you multiply 5 by -1, the result is -5.The minuses gets cancelled here");
+            int positiveNumber = 5;
             int minusOne = -1;
-            int negativeNumber = someNumber * minusOne;
+            int negativeNumber = positiveNumber * minusOne;
             Console.WriteLine("5 * -1 = {0}", negativeNumber);
+            Console.WriteLine();
+            
+            Console.WriteLine("This is also same for negative numbers. For example: -5 * -1 = 5");
+            int negativeFive = -5;
+            int negativeFiveTimesMinusOne = negativeFive * minusOne;
+            Console.WriteLine("-5 * -1 = {0}", negativeFiveTimesMinusOne);
+
             Console.WriteLine("--------------------------------------------------------------------");
+            int x = 3 + 2 + -5 - 3;
+            Console.WriteLine("3 + 2 + -5 - 3 : results in " + x);
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Even number of negative signs is equal to a positive sign");
+            Console.WriteLine("Odd number of negative signs is equal to a negative sign");
+            Console.WriteLine("Here is an example:");
+            Console.WriteLine(- - -2);
+            Console.WriteLine(- - - -2);
+            Console.WriteLine("When cancelled in pairs, even number of signs get striked out, the last odd numbered sign remains");
+
+
+
 
 
 
